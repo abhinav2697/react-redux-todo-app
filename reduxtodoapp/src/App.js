6e3d2793d453fdc5ef2d17b1d72454d8aa6f2c92
abhinav2697 <abhinav2697@gmail.com>
2024-08-06 22:22:19 +0530

    new files and features added

---
+++ reduxtodoapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Navbar.jsx';
+import Todo from './Todo.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reduxtodoapp/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import './Navbar.css'
+const Navbar = () => {
+  return (
+      <div className='navbar'>
+          React-Redux TodoList
+      </div>
+  )
+}
+
+export default Navbar
